43c43aa0650c024214b6f9df29d252872b94befd
sumeyyeyegen <sumeyyeyegen746@gmail.com>
2021-01-26 03:04:48 +0300

    updated.

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,8 @@ const app = require("express")();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
+http.listen(process.env.PORT || 4000, () => {
+    console.log('listening on *:4000');
 });
 
 app.get('/', (req, res) => {
